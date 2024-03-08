2c4aaf65bf20d7b75ed836485626afccf9b719eb
Emmanuel Sosa <126711302+Emmanuel031@users.noreply.github.com>
2024-03-08 11:26:54 -0400

    mini-proyecto: contador
    Contador hecho con HTML CSS y JavaScript

---
+++ contador.js
@@ -0,0 +1,31 @@
+let incrementar = document.querySelector('.Incrementar'),
+decrementar = document.querySelector('.Decrementar'),
+reiniciar = document.querySelector('.Reiniciar'),
+numero = document.querySelector('.numero');
+
+
+
+incrementar.addEventListener('click', () =>{
+    let contenido = parseInt(numero.innerText);
+    contenido += 1;
+    numero.innerText = contenido;
+
+    if(contenido >= 0) {
+        numero.style.color = 'blue';
+    }
+})
+
+decrementar.addEventListener('click', () =>{
+    let contenido = parseInt(numero.innerText);
+    contenido -= 1;
+
+    if(contenido < 0) {
+        numero.style.color = 'red';
+    }
+
+    numero.innerText = contenido;
+})
+
+reiniciar.addEventListener('click', () =>{
+    numero.innerText = '0';
+})
